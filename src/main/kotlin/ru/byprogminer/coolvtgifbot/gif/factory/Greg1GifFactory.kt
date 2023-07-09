cc52b1d2524e92fe98c53382a7f7d300c6c2cd24
Eridan Domoratskiy <eridan200@mail.ru>
2023-07-09 19:03:39 +0300

    Add greg1 gif

---
+++ src/main/kotlin/ru/byprogminer/coolvtgifbot/gif/factory/Greg1GifFactory.kt
@@ -0,0 +1,29 @@
+package ru.byprogminer.coolvtgifbot.gif.factory
+
+import org.bytedeco.javacv.FFmpegFrameGrabber
+import org.springframework.core.io.ClassPathResource
+import org.springframework.stereotype.Service
+import ru.byprogminer.coolvtgifbot.utils.PlaceTextOptions
+import ru.byprogminer.coolvtgifbot.utils.placeText
+import java.awt.Color
+import java.nio.file.Path
+
+
+@Service
+class Greg1GifFactory : AbstractGifFactory(
+    ClassPathResource("gif/greg1.mp4"),
+) {
+
+    override fun createOriginalGif(text: String, resultPath: Path) {
+        FFmpegFrameGrabber(originalGif.inputStream).placeText(PlaceTextOptions(
+            text = text,
+            x = 5,
+            y = 25,
+            width = 470,
+            height = 65,
+            color = Color.WHITE,
+            borderWidth = 5,
+            backgroundColor = Color.BLACK,
+        ), resultPath)
+    }
+}
